02046ff55201be23cc933b55afc694a7268ced8d
gabriel-fresan <gabrielfrsan@proton.me>
2024-05-26 19:41:32 -0300

    remoção de informações desnecessárias do model Cliente

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -2,33 +2,21 @@ package Model;
 
 public class Cliente extends Pessoa {
     
-    
-    private int pessoa_id;
     private String telefone;
     private String endereco;
 
     public Cliente() {
     }
 
-    public Cliente(int pessoa_id, String telefone, String endereco) {
-        this.pessoa_id = pessoa_id;
+    public Cliente(String telefone, String endereco) {
         this.telefone = telefone;
         this.endereco = endereco;
     }
 
-    public Cliente(int pessoa_id, String telefone, String endereco, int id, String nome, String email) {
+    public Cliente(String telefone, String endereco, int id, String nome, String email) {
         super(id, nome, email);
-        this.pessoa_id = pessoa_id;
         this.telefone = telefone;
         this.endereco = endereco;
-    }
-
-    public int getPessoa_id() {
-        return pessoa_id;
-    }
-
-    public void setPessoa_id(int pessoa_id) {
-        this.pessoa_id = pessoa_id;
     }
 
     public String getTelefone() {
