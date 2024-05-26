378f5a5ed565c3d800bbd0d448d31c059a76ce5b
gabriel-fresan <gabrielfrsan@proton.me>
2024-05-26 18:29:20 -0300

    Simplificação do modelo Funcionário

--- src/Model/Funcionario.java
+++ src/Model/Funcionario.java
@@ -3,32 +3,21 @@ package Model;
 public class Funcionario extends Pessoa {
     
     
-    private int pessoa_id;
     private String cargo;
     private double salario;
 
     public Funcionario() {
     }
 
-    public Funcionario(int pessoa_id, String cargo, double salario) {
-        this.pessoa_id = pessoa_id;
+    public Funcionario(String cargo, double salario) {
         this.cargo = cargo;
         this.salario = salario;
     }
 
-    public Funcionario(int pessoa_id, String cargo, double salario, int id, String nome, String email) {
+    public Funcionario(String cargo, double salario, int id, String nome, String email) {
         super(id, nome, email);
-        this.pessoa_id = pessoa_id;
         this.cargo = cargo;
         this.salario = salario;
-    }
-
-    public int getPessoa_id() {
-        return pessoa_id;
-    }
-
-    public void setPessoa_id(int pessoa_id) {
-        this.pessoa_id = pessoa_id;
     }
 
     public String getCargo() {
@@ -47,43 +36,21 @@ public class Funcionario extends Pessoa {
         this.salario = salario;
     }
 
+    public void alterarCargo(String novoCargo) {
+        this.cargo = novoCargo;
+    }
+    
+    public double calcularBonus(double percentual) {
+        return this.salario * percentual / 100;
+    }
     
     //metodos
     public void imprimirDadosFuncionario() {
-    System.out.println("ID: " + getId());
-    System.out.println("Nome: " + getNome());
-    System.out.println("Email: " + getEmail());
-    System.out.println("Pessoa ID: " + pessoa_id);
-    System.out.println("Cargo: " + cargo);
-    System.out.println("Salário: " + salario);
-}
-    public void alterarCargo(String novoCargo) {
-    this.cargo = novoCargo;
+        System.out.println("ID: " + getId());
+        System.out.println("Nome: " + getNome());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salário: " + salario);
     }
-    
-     public double calcularBonus(double percentual) {
-        return this.salario * percentual / 100;
+
 }
-
-
-
-    
-    
-    
-    
-  
-    }
-    
-    
-    
-    
-    
-
-   
-
-    
-    
-            
-
-
-
